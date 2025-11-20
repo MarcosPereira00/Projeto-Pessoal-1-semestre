a7d7234e463f72772695196e865f50e9876c3ca9
MarcosPereira00 <marcos.pereira@sptech.school>
2025-11-19 22:08:31 -0300

    Javascript Tela de login, cadastro e quiz, +Der e script BD

--- Banco de Dados/Script Quiz.sql
+++ Banco de Dados/Script Quiz.sql
@@ -2,22 +2,36 @@ create database ProjetoPessoal;
 use ProjetoPessoal;
 
 create table Usuarios
-(idUsuarios int primary key auto_increment,
+(idUsuario int primary key auto_increment,
 nome varchar(45), sobrenome varchar(45),
 email varchar(45), senha varchar(45));
+
+
 
 create table Quiz 
 (idQuiz int primary key auto_increment,
 perguntas varchar(60));
 
-create table Respostas
-(idResposta int,
+
+create table Tentativa
+(idTentativa int ,
 fkUsuarios int,
 fkQuiz int,
-primary key (idResposta, fkUsuarios, fkQuiz),
-constraint RespostasUsuarios
+alternativaPirata int,
+alternativaMarinha int,
+alternativaRevolucionarios int,
+dtHora timestamp default current_timestamp,
+primary key (idTentativa, fkUsuarios, fkQuiz),
+constraint TentativaUsuarios
 foreign key (fkUsuarios)
-references Usuarios(idUsuarios),
+references Usuarios(idUsuario),
 foreign key (fkQuiz)
 references Quiz(idQuiz));
+
+
+
+select * from Usuarios;
+select * from Tentativa;
+select * from Quiz;
+
 
